e159f11440739a9ec8d981844cb6a109e00b1f6c
adi3120 <aditya.yadav.cs.20@ggits.net>
2023-04-08 02:01:27 +0530

    Added requirements.txt

--- utils/scrape.py
+++ utils/scrape.py
@@ -1,7 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
-from timeout_decorator import timeout
-import signal 
 session = requests.session()
 session.proxies = {'http':  'socks5h://127.0.0.1:9050',
                    'https': 'socks5h://127.0.0.1:9050'}
